1f7bbb6069e7e2a82b574532783ef8d16d74f497
Breyanna Vass <breyannavass@gmail.com>
2019-10-10 19:26:38 -0400

    fixing up empty cart and the cart display

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 import Title from "../Title";
+import { ProductConsumer } from "../../context";
 
 import CartColumn from "./CartColumn";
+import EmptyCart from "./EmptyCart";
 
 export default class Cart extends Component {
   render() {
     return (
       <section>
-        <Title name="your" title="cart" />
-        <CartColumn />
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColumn />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
       </section>
     );
   }
