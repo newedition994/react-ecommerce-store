4cd4e6d8d8ec760a41fe4b0fa852a98027e52f2c
Breyanna Vass <breyannavass@gmail.com>
2019-10-10 14:20:21 -0400

    adding style components to decorate the button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import logo from "../logo.svg";
+import styled from "styled-components";
 
 export default class Navbar extends Component {
   render() {
@@ -21,13 +22,33 @@ https://www.iconfinder.com/Makoto_msk */}
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <span>
+          <ButtonContainer>
+            <span className="mr-2">
               My Cart <i className="fas fa-cart-plus" />
             </span>
-          </button>
+          </ButtonContainer>
         </Link>
       </nav>
     );
   }
 }
+
+const ButtonContainer = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--lightBlue);
+  color: var(--lightBlue);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    background: var(--lightBlue);
+    color: var(--mainBlue);
+  }
+  &:focus {
+    outline: none;
+  }
+`;
