483eddc1898433096f2ce6365471693b9c0f0242
Breyanna Vass <breyannavass@gmail.com>
2019-10-10 18:43:11 -0400

    adding columns for the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import Title from "../Title";
+
+import CartColumn from "./CartColumn";
 
 export default class Cart extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from Cart </h3>
-      </div>
+      <section>
+        <Title name="your" title="cart" />
+        <CartColumn />
+      </section>
     );
   }
 }
