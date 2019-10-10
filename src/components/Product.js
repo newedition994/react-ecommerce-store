83d49065b40ce0b2bdcb2adf548617b4ce740788
Breyanna Vass <breyannavass@gmail.com>
2019-10-10 15:55:23 -0400

    adjusting the color for the button to add

--- src/components/Product.js
+++ src/components/Product.js
@@ -85,6 +85,15 @@ const ProductWrapper = styled.div`
     border: none;
     color: var(--mainWhite);
     font-size; 1.4rem;
-    border-radius: 0.5 0 0 0;
+    border-radius: 0.5rem 0 0 0;
+    transform: translate(100%, 100%);
+    transition: all 1s linear;
+  }
+  .img-container:hover .cart-btn {
+    transform: translate(0, 0);
+  }
+  .cart-btn:hover {
+    color: var(--mainBlue);
+    cursor: pointer;
   }
 `;
